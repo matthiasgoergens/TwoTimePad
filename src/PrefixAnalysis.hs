86a162345fce470fd568d3947bf7f62b5bcb8131
Matthias Gorgens <matthias.goergens@gmail.com>
2013-05-05 18:33:23 +0800

    PrefixAnalysis

---
+++ src/PrefixAnalysis.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE TupleSections #-}
+
+import System.IO.MMap
+
+import System.Environment
+-- import Control.Monad
+
+import qualified Data.ByteString as BS
+-- import qualified Data.ByteString.Char8 as BS8
+-- import Data.Trie as T
+import Data.Trie.Convenience as T
+
+-- import System.Environment
+
+
+readCorpus :: FilePath -> IO BS.ByteString
+readCorpus corpusName = mmapFileByteString corpusName Nothing
+
+main :: IO ()
+main = do
+  (corpusName:_) <- getArgs
+  print . show . T.fromListWith (+) . fmap (,(1 :: Int)) . fmap (BS.take 1) . BS.tails =<< readCorpus corpusName
+
