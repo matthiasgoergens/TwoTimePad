3e4b03dac06413b395d0bf043c87197ac93d9d01
Matthias Gorgens <matthias.gorgens@oaknorth.ai>
2019-12-30 06:43:00 +0000

    Only one batch norm per resnet

--- two_time_pad.py
+++ two_time_pad.py
@@ -286,14 +286,16 @@ def make_model(hparams):
     def makeResNet(i):
       resInputMe = Input(name = 'res_inputMe', shape=(n,resSize,))
       resInputDu = Input(name = 'res_inputDu', shape=(n,resSize,))
+      resInputMeN = TimeDistributed(BatchNormalization())(resInputMe)
+      resInputDuN = TimeDistributed(BatchNormalization())(resInputDu)
       convedBroad = (
-          TimeDistributed(BatchNormalization())(
+          ( # TimeDistributed(BatchNormalization())(
           relu()(
           Conv1D(
             filters=6*46, kernel_size=15, padding='same',
             kernel_initializer=keras.initializers.he_normal(seed=None),
             )(
-          TimeDistributed(BatchNormalization())(
+          ( # TimeDistributed(BatchNormalization())(
           relu()(
           Conv1D(
             filters=4*46, kernel_size=1,
@@ -301,15 +303,15 @@ def make_model(hparams):
             )(
           # TimeDistributed(keras.layers.AlphaDropout(0.5))(
           ( # SpatialDropout1D(rate=hparams[HP_DROPOUT])(
-            resInputMe))))))))
+            resInputMeN))))))))
       convedNarrow = (
-          TimeDistributed(BatchNormalization())(
+          ( # TimeDistributed(BatchNormalization())(
           relu()(
           Conv1D(
             filters=6*46, kernel_size=5, padding='same',
             kernel_initializer=keras.initializers.he_normal(seed=None),
             )(
-          TimeDistributed(BatchNormalization())(
+          ( # TimeDistributed(BatchNormalization())(
           relu()(
           Conv1D(
             filters=4*46, kernel_size=1,
@@ -317,16 +319,16 @@ def make_model(hparams):
             )(
           # TimeDistributed(keras.layers.AlphaDropout(0.5))(
           ( # SpatialDropout1D(rate=hparams[HP_DROPOUT])(
-            resInputMe))))))))
+            resInputMeN))))))))
       innerConved =(
-          TimeDistributed(BatchNormalization())(
+          ( # TimeDistributed(BatchNormalization())(
           relu()(
           Add(name='resOutput')([resInputMe,
             Conv1D(filters=resSize, kernel_size=1,
               kernel_initializer=keras.initializers.he_normal(seed=None),
             )(
             ( # SpatialDropout1D(rate=hparams[HP_DROPOUT])(
-            concatenate([resInputMe, convedNarrow, convedBroad, resInputDu])))]))))
+            concatenate([resInputMeN, convedNarrow, convedBroad, resInputDuN])))]))))
       return Model([resInputMe, resInputDu], [innerConved], name=f'resnet{i}')
 
     for i in range(hparams[HP_HEIGHT]):
