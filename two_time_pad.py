ae4afbaec2e1735b47723bd8e526f27a4a0edaa6
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-01-02 13:19:03 +0000

    Thin

--- two_time_pad.py
+++ two_time_pad.py
@@ -364,8 +364,8 @@ def make_mode_global_local(hparams):
 
     def make_block(global_state):
         width = 1 + 2 * 2
-        local_dims = 2*46
-        more_global = 46
+        local_dims = 46
+        more_global = 23
 
         num_layers = height
 
@@ -420,12 +420,12 @@ def make_mode_global_local(hparams):
 l = 100
 hparams = {
     HP_DROPOUT: 0.0,
-    HP_HEIGHT: 10,
+    HP_HEIGHT: 20,
     HP_WINDOW: l,
     HP_resSize: 4 * 46,
 }
 
-weights_name = "glocal-10.h5"
+weights_name = "glocal-20-thin.h5"
 
 
 def main():
