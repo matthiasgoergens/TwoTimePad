89b4147ca963cdb57520a3298dbd1a62656e0958
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-01-04 05:16:41 +0000

    Run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@
 set -ex
 NAME="$(grep '^weights_name *=' two_time_pad.py | grep -o '".*"')"
 mkdir -p "runs/${NAME}"
-cp two_time_pad.py  "run/${NAME}/"
+cp two_time_pad.py  "runs/${NAME}/"
+
+git add "runs/${NAME}"
 git commit -m "Run: ${NAME}" two_time_pad.py "runs/${NAME}"
 docker exec -it interesting_noyce python3 two_time_pad.py "${NAME}"
