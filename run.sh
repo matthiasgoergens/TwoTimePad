1f9d0ff5142ebc7def96ac44d1bb13d12d69a101
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-01-07 08:38:02 +0000

    Allow empty

--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@ mkdir -p "runs/${NAME}"
 cp two_time_pad.py  "runs/${NAME}/"
 
 git add "runs/${NAME}"
-git commit -m "Run: ${NAME}" two_time_pad.py "runs/${NAME}"
+git commit --allow-empty -m "Run: ${NAME}" two_time_pad.py "runs/${NAME}"
 docker exec -it interesting_noyce python3 two_time_pad.py "${NAME}"
