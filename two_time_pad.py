9923d792a64af6ce8b8536dbf09513d9a07236b5
Matthias Gorgens <matthias.gorgens@oaknorth.ai>
2020-01-07 17:29:26 +0800

    Dropout at end

--- two_time_pad.py
+++ two_time_pad.py
@@ -689,6 +689,8 @@ def main():
     # denseCNN-20-random-mixed-pre-activation-shorter-seed-23.h5 best by far.  That's what I'm trying to recreate and improve on.
     # Both-loss at minimum was ~.92 (so single ~0.46) and accuracy was ~86.2%
 
+    # Dropout _after_ all BatchNorm is fine.  Especially drop out just before the end should help.
+
 # Base loss for one side:
 # log(46, 2)
 # 5.523561956057013
