d1e96e36dc61ef6a6b2001e34bde3092c0c1b371
Matthias Gorgens <matthias.gorgens@oaknorth.ai>
2020-01-09 11:36:39 +0800

    Remove CRelu

--- two_time_pad.py
+++ two_time_pad.py
@@ -205,29 +205,6 @@ METRIC_ACCURACY = "accuracy"
 relu = ft.partial(tf.keras.layers.PReLU, shared_axes=[1])
 crelu = lambda: tf.nn.crelu
 
-class CRelu(Layer):
-    def __init__(self, axis=-1, **kwargs):
-        self.axis = axis
-        super(CRelu, self).__init__(**kwargs)
-
-    def build(self, input_shape):
-        super(CRelu, self).build(input_shape)
-
-    def call(self, x):
-        x = tf.nn.crelu(x, axis=self.axis)
-        return x
-
-    def compute_output_shape(self, input_shape):
-        output_shape = list(input_shape)
-        output_shape[-1] = output_shape[-1] * 2
-        output_shape = tuple(output_shape)
-        return output_shape
-
-    def get_config(self, input_shape):
-        config = {'axis': self.axis, }
-        base_config = super(CReLU, self).get_config()
-        return dict(list(base_config.items()) + list(config.items()))
-
 def plus(a, b):
     if a is None:
         return b
