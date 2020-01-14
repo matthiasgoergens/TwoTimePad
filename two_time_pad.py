381976474125fdf1c3c3cdc3b8f64ace120adb34
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-01-14 08:33:50 +0000

    Run: "conv-0-deviation-as-metric-debug.h5"

--- two_time_pad.py
+++ two_time_pad.py
@@ -318,8 +318,9 @@ def fShift(tensors):
         key = Softmax(dtype='float32')(key)
 
     cce = tf.keras.backend.categorical_crossentropy
+    cce = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
     # return cce(clear, key, from_logits=False) + cce(key, clear, from_logits=False)
-    return cce(clear, clear, from_logits=False) + cce(key, key, from_logits=False)
+    return cce(clear, clear) + cce(key, key)
 
 def shiftShapes(inputShapes):
     [clearShape, keyShape, shiftsShapes] = inputShapes
