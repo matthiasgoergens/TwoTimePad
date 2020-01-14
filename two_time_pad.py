3d4aafad6e0d219858b1b061c23c00bbe971756f
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-01-14 08:57:53 +0000

    Run: "conv-0-deviation-as-metric-debug-x.h5"

--- two_time_pad.py
+++ two_time_pad.py
@@ -317,8 +317,7 @@ def ShiftLayer(clear, key, shifts):
     # cce = tf.keras.backend.categorical_crossentropy
     cce = tf.keras.losses.CategoricalCrossentropy
     # cce = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False)
-    return 0
-    return cce(name="a")(clear, clear) + cce(name="b")(key, key)
+    return  0 * (cce(name="a")(clear, clear) + cce(name="b")(key, key))
     # return cce(clear, clear) + cce(key, key)
 
 # Resnet.
