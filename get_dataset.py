c9a18fcc762018e24389b464a556a22ffe52afd3
Matthias Görgens <matthias.goergens@gmail.com>
2021-05-20 15:29:43 +0800

    Format

--- get_dataset.py
+++ get_dataset.py
@@ -5,12 +5,14 @@ import itertools as it
 import random
 import re
 import sys
+import time
 from datetime import datetime
 from pprint import pprint
-import time
 
 import numpy as np
 import tensorflow as tf
+import tensorflow_addons as tfa
+from keras.callbacks import *
 from tensorboard.plugins.hparams import api as hp
 from tensorflow import keras
 from tensorflow.keras.layers import (
@@ -33,9 +35,6 @@ from tensorflow.keras.layers import (
     concatenate,
 )
 from tensorflow.keras.models import Model, Sequential
-
-import tensorflow_addons as tfa
-from keras.callbacks import *
 from tensorflow_addons.layers import Maxout, Sparsemax
 
 alpha = " ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.?,-:;'()".lower()
@@ -155,12 +154,11 @@ class TwoTimePadSequence(keras.utils.Sequence):
         self.window = window
 
 
-
 # class ArtificialDataset(tf.data.Dataset):
 #       def _generator(num_samples, window, batch_size):
 #           # Opening the file
 #           my_text = clean(load())
-          
+
 
 #           for sample_idx in range(num_samples):
 #               # Reading data (line, record) from the file
@@ -187,53 +185,57 @@ def cipher_for_predict():
     # print(c2)
     return tf.convert_to_tensor([sub(c1, c2)])
 
+
 def round_to(x, n):
-  return (x // n) * n
+    return (x // n) * n
+
 
 def make(window, text):
-  (size,) = text.shape
-  start = random.randrange(window)
-  return tf.reshape(tf.slice(text, [start], [round_to(size - window, window)]), (-1, window))
+    (size,) = text.shape
+    start = random.randrange(window)
+    return tf.reshape(
+        tf.slice(text, [start], [round_to(size - window, window)]), (-1, window)
+    )
 
-mtext = tf.convert_to_tensor(text, dtype='int8')
+
+mtext = tf.convert_to_tensor(text, dtype="int8")
+
 
 def main():
-  window = 60
-  x = make(window, mtext)
-  y = make(window, mtext)
-  print("-----------------------------")
-  for i in range(10):
-    xx = tf.random.shuffle(x)
-    yy = tf.random.shuffle(y)
-    print(xx.shape, yy.shape)
-    print(
-      (xx-yy)%46, [xx, yy]
-      )
+    window = 60
+    x = make(window, mtext)
+    y = make(window, mtext)
+    print("-----------------------------")
+    for i in range(10):
+        xx = tf.random.shuffle(x)
+        yy = tf.random.shuffle(y)
+        print(xx.shape, yy.shape)
+        print((xx - yy) % 46, [xx, yy])
 
 
 def x():
-  window_size = 61
-  shuffle_window = 10**5
+    window_size = 61
+    shuffle_window = 10 ** 5
 
-  (size,) = mtext.shape
-  print(mtext)
-  ds = tf.data.Dataset.from_tensors(mtext)
-  print(ds)
-  # Consider smaller shift.
-  dsw = ds.window(window_size, shift=1, drop_remainder=True)
-  dswA = dsw.shuffle(shuffle_window)
-  dswB = dsw.shuffle(shuffle_window)
-  print("dswA: ", end='')
-  print(dswA)
-  print()
-  # y = (dswA - dswB) % 46
-  y = tf.data.Dataset.zip((dswA, dswB))
-  print(f"zip: {y}")
-  for x in y:
-    print (x)
+    (size,) = mtext.shape
+    print(mtext)
+    ds = tf.data.Dataset.from_tensors(mtext)
+    print(ds)
+    # Consider smaller shift.
+    dsw = ds.window(window_size, shift=1, drop_remainder=True)
+    dswA = dsw.shuffle(shuffle_window)
+    dswB = dsw.shuffle(shuffle_window)
+    print("dswA: ", end="")
+    print(dswA)
+    print()
+    # y = (dswA - dswB) % 46
+    y = tf.data.Dataset.zip((dswA, dswB))
+    print(f"zip: {y}")
+    for x in y:
+        print(x)
 
 
 if __name__ == "__main__":
-  x()
-  #while True:
-  #  main()
+    x()
+    # while True:
+    #  main()
--- two.py
+++ two.py
@@ -1,15 +1,12 @@
 import pathlib
+import random
 import re
+from pprint import pprint
 
+import numpy as np
 import tensorflow
 import tensorflow as tf
 from tensorflow import keras
-
-
-import pathlib
-import numpy as np
-import random
-from pprint import pprint
 
 # Train with longer later.
 # n = 25
@@ -25,217 +22,233 @@ np.set_printoptions(precision=4)
 #   train
 #   Optional: show current results.
 
-alpha =    " ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.?,-:;'()".lower()
-alphaRE = alpha.replace("-","\\-")
+alpha = " ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.?,-:;'()".lower()
+alphaRE = alpha.replace("-", "\\-")
 
 assert len(alpha) == 46
 
+
 def load():
-  text = ' '.join(f.open('r').read() for f in pathlib.Path('data').glob('*.txt')).lower()
-  text = re.sub('\s+', ' ', text)
-  text = re.sub(f'[^{alphaRE}]', '', text)
-  return text
+    text = " ".join(
+        f.open("r").read() for f in pathlib.Path("data").glob("*.txt")
+    ).lower()
+    text = re.sub("\s+", " ", text)
+    text = re.sub(f"[^{alphaRE}]", "", text)
+    return text
+
 
 def add(clear, key):
-  return [(a+b) % len(alpha) for a, b in zip(clear, key)]
+    return [(a + b) % len(alpha) for a, b in zip(clear, key)]
+
 
 def sub(cipher, key):
-  return [(a-b) % len(alpha) for a, b in zip(cipher, key)]
+    return [(a - b) % len(alpha) for a, b in zip(cipher, key)]
+
 
 def clean(text):
-  t = {c: i for i, c in enumerate(alpha)}
-  return [t[c] for c in text]
+    t = {c: i for i, c in enumerate(alpha)}
+    return [t[c] for c in text]
+
 
 def toChar(numbers):
-  return ''.join(alpha[i] for i in numbers)
+    return "".join(alpha[i] for i in numbers)
+
 
 # toChar
 # (5, 125, 46)
 def toChars(tensor):
-  (linesNums, charNum, alphaNum) = tensor.shape
-  output = []
-  # TODO: use gather https://www.tensorflow.org/api_docs/python/tf/gather?version=stable
-  assert alphaNum == len(alpha)
-  for lineNum in range(linesNums):
-    chars = []
-    for cN in range(charNum):
-      (_, char) = max([(tensor[lineNum, cN, alphaN], alphaN) for alphaN in range(alphaNum)])
-      chars.append(char)
-    output.append(toChar(chars))
-  return output
+    (linesNums, charNum, alphaNum) = tensor.shape
+    output = []
+    # TODO: use gather https://www.tensorflow.org/api_docs/python/tf/gather?version=stable
+    assert alphaNum == len(alpha)
+    for lineNum in range(linesNums):
+        chars = []
+        for cN in range(charNum):
+            (_, char) = max(
+                [(tensor[lineNum, cN, alphaN], alphaN) for alphaN in range(alphaNum)]
+            )
+            chars.append(char)
+        output.append(toChar(chars))
+    return output
+
 
 def toChars_labels(labels):
-  (linesNums, charNum) = labels.shape
-  output = []
-  for lineNum in range(linesNums):
-    chars = []
-    for cN in range(charNum):
-      chars.append(labels[lineNum, cN])
-    output.append(toChar(chars))
-  return output
+    (linesNums, charNum) = labels.shape
+    output = []
+    for lineNum in range(linesNums):
+        chars = []
+        for cN in range(charNum):
+            chars.append(labels[lineNum, cN])
+        output.append(toChar(chars))
+    return output
 
 
 def prepare(clear, key):
-  assert len(clear) == len(key), (clear, key)
+    assert len(clear) == len(key), (clear, key)
 
-  depth = len(alpha)
+    depth = len(alpha)
 
-  # label = clear[len(clear)//2:len(clear)//2+1]
-  cipher = add(clear, key)
-  return (cipher, clear)
+    # label = clear[len(clear)//2:len(clear)//2+1]
+    cipher = add(clear, key)
+    return (cipher, clear)
 
 
 def sample(text, l):
-  start = random.randrange(len(text) - l)
-  return text[start:start+l]
+    start = random.randrange(len(text) - l)
+    return text[start : start + l]
+
 
 def samples(text, batch_size, l):
-  ciphers = []
-  keys = []
-  labels = []
-  for _ in range(batch_size):
-    clear = sample(text, l)
-    key = sample(text, l)
+    ciphers = []
+    keys = []
+    labels = []
+    for _ in range(batch_size):
+        clear = sample(text, l)
+        key = sample(text, l)
 
-    (cipher, label) = prepare(clear, key)
-    # print(toChar(clear))
-    # print(toChar(key))
-    # print(toChar(add(clear, key)))
-    # print(toChar(sub(add(clear, key), key)))
-    ciphers.append(cipher)
-    labels.append(label)
-    keys.append(key)
-  one_hot_ciphers = tf.convert_to_tensor(ciphers)
-  one_hot_labels = tf.convert_to_tensor(labels)
-  one_hot_keys = tf.convert_to_tensor(keys)
-  # print(one_hot_ciphers.shape)
-  # print(one_hot_labels.shape)
-  # print(len(one_hot_labels))
-  return (one_hot_ciphers,
-         one_hot_labels,
-         one_hot_keys)
+        (cipher, label) = prepare(clear, key)
+        # print(toChar(clear))
+        # print(toChar(key))
+        # print(toChar(add(clear, key)))
+        # print(toChar(sub(add(clear, key), key)))
+        ciphers.append(cipher)
+        labels.append(label)
+        keys.append(key)
+    one_hot_ciphers = tf.convert_to_tensor(ciphers)
+    one_hot_labels = tf.convert_to_tensor(labels)
+    one_hot_keys = tf.convert_to_tensor(keys)
+    # print(one_hot_ciphers.shape)
+    # print(one_hot_labels.shape)
+    # print(len(one_hot_labels))
+    return (one_hot_ciphers, one_hot_labels, one_hot_keys)
+
 
 # relu = keras.activations.relu # (alpha=0.1)
 relu = lambda: keras.layers.LeakyReLU(alpha=0.1)
 # keras.layers.Embedding
 
-from tensorflow.keras.layers import Embedding, Input, Dense, Dropout, Softmax, GlobalMaxPooling1D, MaxPooling1D, Conv1D, Flatten, concatenate, Bidirectional, LSTM, SimpleRNN
-from tensorflow.keras.models import Sequential, Model
 import tensorflow_addons as tfa
+from tensorflow.keras.layers import (
+    LSTM,
+    Bidirectional,
+    Conv1D,
+    Dense,
+    Dropout,
+    Embedding,
+    Flatten,
+    GlobalMaxPooling1D,
+    Input,
+    MaxPooling1D,
+    SimpleRNN,
+    Softmax,
+    concatenate,
+)
+from tensorflow.keras.models import Model, Sequential
 
 Maxout = tfa.layers.Maxout
 
 # TODO: use convolution over 1d.
 # tf.keras.layers.Conv1D
 def make_model():
-  # Not sure if lambda is necessary to not share weights?
-  dropout = lambda: Dropout(rate=0.1)
-  
-  my_input = Input(shape=(None,), dtype='int32', name="ciphertext")
-  embedded = (
-    dropout()(
-    # Give larger embedding to allow for dropout?
-    Embedding(output_dim=2*len(alpha), input_dim=len(alpha), name="my_embedding")(
-    my_input
-  )))
+    # Not sure if lambda is necessary to not share weights?
+    dropout = lambda: Dropout(rate=0.1)
 
-  # Idea is to bubble up better and better guesses for what letters we have locally.
-  conv = lambda base: Maxout(46*2*2)(dropout()(Conv1D(
-      filters=46*2*2 * 5, kernel_size=5,
-      padding='same', strides=1)(
-        base
-      )))
-  # Not sure whether the eta abstraction is necessary?
-  lstm = lambda base: (
-    Bidirectional(LSTM(46*2*2, dropout=0.1, recurrent_dropout=0.1, return_sequences=True))(
-      base))
-  lstm_ed = (
-    lstm(conv(
-    lstm(conv(
-    lstm(conv(
-    lstm(conv(
-    embedded
-  )))))))))
+    my_input = Input(shape=(None,), dtype="int32", name="ciphertext")
+    embedded = dropout()(
+        # Give larger embedding to allow for dropout?
+        Embedding(output_dim=2 * len(alpha), input_dim=len(alpha), name="my_embedding")(
+            my_input
+        )
+    )
 
-  totes_clear = (
-    Softmax(name="clear")(Conv1D(
-      filters=len(alpha), kernel_size=5,
-      padding='same', strides=1)(
-    lstm_ed
-  )))
-  totes_key = (
-    Softmax(name="key")(Conv1D(
-      filters=len(alpha), kernel_size=5,
-      padding='same', strides=1)(
-    lstm_ed
-  )))
-  totes_cipher = (
-    Softmax(name="cipher_again")(Conv1D(
-      filters=len(alpha), kernel_size=1,
-      padding='same', strides=1)(
-    lstm_ed
-  )))
-  model = Model([my_input], [totes_cipher, totes_clear, totes_key])
+    # Idea is to bubble up better and better guesses for what letters we have locally.
+    conv = lambda base: Maxout(46 * 2 * 2)(
+        dropout()(
+            Conv1D(filters=46 * 2 * 2 * 5, kernel_size=5, padding="same", strides=1)(
+                base
+            )
+        )
+    )
+    # Not sure whether the eta abstraction is necessary?
+    lstm = lambda base: (
+        Bidirectional(
+            LSTM(46 * 2 * 2, dropout=0.1, recurrent_dropout=0.1, return_sequences=True)
+        )(base)
+    )
+    lstm_ed = lstm(conv(lstm(conv(lstm(conv(lstm(conv(embedded))))))))
 
-  model.compile(
-    optimizer='adam',
-    loss='sparse_categorical_crossentropy',
-    metrics=['accuracy'])
-  return model
+    totes_clear = Softmax(name="clear")(
+        Conv1D(filters=len(alpha), kernel_size=5, padding="same", strides=1)(lstm_ed)
+    )
+    totes_key = Softmax(name="key")(
+        Conv1D(filters=len(alpha), kernel_size=5, padding="same", strides=1)(lstm_ed)
+    )
+    totes_cipher = Softmax(name="cipher_again")(
+        Conv1D(filters=len(alpha), kernel_size=1, padding="same", strides=1)(lstm_ed)
+    )
+    model = Model([my_input], [totes_cipher, totes_clear, totes_key])
+
+    model.compile(
+        optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"]
+    )
+    return model
+
 
 import sys
 
-training_size = 2 * 10**4
-test_size = 10**3
+training_size = 2 * 10 ** 4
+test_size = 10 ** 3
 
 
-weights_name = 'my_model_weights-increasing.h5'
-if __name__ == '__main__':
-  model = make_model()
-  try:
-    model.load_weights(weights_name)
-    print("Loaded weights.")
-  except:
-    print("Failed to load weights.")
+weights_name = "my_model_weights-increasing.h5"
+if __name__ == "__main__":
+    model = make_model()
+    try:
+        model.load_weights(weights_name)
+        print("Loaded weights.")
+    except:
+        print("Failed to load weights.")
 
-  print(model)
+    print(model)
 
-  # Starting from higher.
-  import sys
-  l = int(sys.argv[1])
-  while True:
-    print()
-    text = clean(load())
-    print(f"text size: {len(text):,}")
-    model.save_weights(weights_name)
-    print("Saved weights")
-    print(f"Window length: {l}")
+    # Starting from higher.
+    import sys
 
-    print("Predict:")
-    predict_size = 5
-    (ciphers_p, labels_p, keys_p) = samples(text, predict_size, l)
-    [pred_cipher, pred_label, pred_key] = (model.predict(ciphers_p))
-    # clear, key, prediction
-    pprint(list(zip(
-        toChars_labels(ciphers_p),
-        toChars(pred_cipher),
+    l = int(sys.argv[1])
+    while True:
+        print()
+        text = clean(load())
+        print(f"text size: {len(text):,}")
+        model.save_weights(weights_name)
+        print("Saved weights")
+        print(f"Window length: {l}")
 
-        predict_size * [l * " "],
-        toChars_labels(labels_p),
-        toChars(pred_label),
+        print("Predict:")
+        predict_size = 5
+        (ciphers_p, labels_p, keys_p) = samples(text, predict_size, l)
+        [pred_cipher, pred_label, pred_key] = model.predict(ciphers_p)
+        # clear, key, prediction
+        pprint(
+            list(
+                zip(
+                    toChars_labels(ciphers_p),
+                    toChars(pred_cipher),
+                    predict_size * [l * " "],
+                    toChars_labels(labels_p),
+                    toChars(pred_label),
+                    predict_size * [l * " "],
+                    toChars_labels(keys_p),
+                    toChars(pred_key),
+                )
+            ),
+            width=200,
+        )
 
-        predict_size * [l * " "],
-        toChars_labels(keys_p),
-        toChars(pred_key))),
-      width=200)
+        (ciphers_t, labels_t, keys_t) = samples(text, test_size, l)
+        print("Eval:")
+        model.evaluate(ciphers_t, [ciphers_t, labels_t, keys_t], verbose=2)
 
-    (ciphers_t, labels_t, keys_t) = samples(text, test_size, l)
-    print("Eval:")
-    model.evaluate(ciphers_t,  [ciphers_t, labels_t, keys_t], verbose=2)
-
-
-    print("Training:")
-    (ciphers, labels, keys) = samples(text, training_size, l)
-    model.fit(ciphers, [ciphers, labels, keys])
-    l += 1
-
+        print("Training:")
+        (ciphers, labels, keys) = samples(text, training_size, l)
+        model.fit(ciphers, [ciphers, labels, keys])
+        l += 1
