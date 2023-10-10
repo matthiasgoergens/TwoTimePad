f03db4c7c358dda41787a7f16ca70e1f14e890c0
Matthias Goergens <matthias.goergens@gmail.com>
2023-10-10 18:50:57 +0800

    No docker

--- run.sh
+++ run.sh
@@ -6,4 +6,5 @@ cp two_time_pad.py  "runs/${NAME}/"
 
 git add "runs/${NAME}"
 git commit --allow-empty -m "Run: ${NAME}" two_time_pad.py "runs/${NAME}"
-docker exec -it mystifying_germain python3 two_time_pad.py "${NAME}"
+python two_time_pad.py "${NAME}"
+# docker exec -it mystifying_germain python3 two_time_pad.py "${NAME}"
