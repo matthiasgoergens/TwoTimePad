c9e1d664fbb01ca518feaca7665d60f6aa09547c
Matthias Goergens <matthias.goergens@gmail.com>
2023-10-10 19:16:34 +0800

    Run: "2023-learn-rate-dropout-0_01-cool-down.h5"

--- runs/"2023-learn-rate-dropout-0_01-cool-down.h5"/two_time_pad.py
+++ runs/"2023-learn-rate-dropout-0_01-cool-down.h5"/two_time_pad.py
@@ -282,7 +282,7 @@ def make_model_simple(hparams):
 
     model.compile(
         # optimizer=tf.optimizers.Adam(learning_rate=0.001 / 4),
-        optimizer=tf.optimizers.Adam(learning_rate=0.01),
+        optimizer=tf.optimizers.Adam(learning_rate=0.001 * 4),
         # optimizer=tf.optimizers.Adam(),
         # optimizer=tf.keras.optimizers.experimental.Nadam(),
         loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
