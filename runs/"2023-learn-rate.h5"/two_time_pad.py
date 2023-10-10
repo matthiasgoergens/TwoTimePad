0de858538dbef1d0939858eda32ac87abbd1e777
Matthias Goergens <matthias.goergens@gmail.com>
2023-10-10 18:53:46 +0800

    Run: "2023-learn-rate.h5"

--- runs/"2023-learn-rate.h5"/two_time_pad.py
+++ runs/"2023-learn-rate.h5"/two_time_pad.py
@@ -286,7 +286,7 @@ def make_model_simple(hparams):
 
     model.compile(
         optimizer=tf.optimizers.Adam(learning_rate=0.001 / 2),
-        optimizer=tf.optimizers.Adam(),
+        # optimizer=tf.optimizers.Adam(),
         # optimizer=tf.keras.optimizers.experimental.Nadam(),
         loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
         # loss_weights={'clear': 1/2, 'key': 1/2},
